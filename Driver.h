9120d24c81b75680f53eedd38ba574d9e1b4e323
DucksFoLif <justinlb3003@gmail.com>
2024-12-02 15:12:30 -0800

    Base level stuff just to get a template of what we need to work on

---
+++ Driver.h
@@ -0,0 +1,12 @@
+#ifndef DRIVER_H
+#define DRIVER_H
+
+class Driver
+{
+  
+};
+
+
+
+
+#endif // DRIVER_H
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Driver.h"
+using namespace std;
+
+int main()
+{
+  initiateMenu();
+}
