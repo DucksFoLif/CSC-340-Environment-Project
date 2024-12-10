89d2d2ac4e2ba58f24aa1e01c1e32091e057e976
Geist-Spieler <sophachord@gmail.com>
2024-12-10 00:16:00 -0800

    progress tues night, probably won't compile

---
+++ Node.cpp
@@ -0,0 +1,38 @@
+#include "Node.h"
+
+Node::Node(Animal* data){
+    this->data = data;
+    this->next = nullptr;
+    this->prev = nullptr;
+}
+Node::Node(){
+   this->data = new Animal();
+   this->next = nullptr;
+   this->prev = nullptr;
+}
+
+Node::~Node() {}
+
+Animal* Node::getData() const {
+    return data;
+}
+
+void Node::setData(Animal* data) {
+    data = data;
+}
+
+Node* Node::getNext() const {
+    return next;
+}
+
+void Node::setNext(Node* nextNode) {
+    next = nextNode;
+}
+
+Node* Node::getPrev() const {
+    return prev;
+}
+
+void Node::setPrev(Node* prevNode) {
+    prev = prevNode;
+}
---
+++ Node.h
@@ -0,0 +1,25 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include <string>
+#include "Animal.h"
+
+class Node {
+private:
+    Animal* data;
+    Node* next;
+    Node* prev;
+
+public:
+    Node(Animal* data);
+    Node();
+    ~Node();
+    Animal* getData() const;
+    void setData(Animal* data);
+    Node* getNext() const;
+    void setNext(Node* nextNode);
+    Node* getPrev() const;
+    void setPrev(Node* prevNode);
+};
+
+#endif
